0c5da1feb3671f116d39e9fbeacd38ee7136b8fa
Karim Yassine <karimwebdevelopment@gmail.com>
2021-08-03 17:10:13 -0300

    addded toggler event listener

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,6 @@
 import "../styles/index.scss";
+import handleMenu from "./handleMenu";
+
+const toggleMenuBtn = document.querySelector(".header__toggle-menu");
+
+toggleMenuBtn.addEventListener("click", () => handleMenu(toggleMenuBtn));
